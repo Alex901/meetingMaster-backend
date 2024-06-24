c34f203e7dbc89876288c29c15cd926b5f40ea1b
Alexander Winberg <alexander.winberg.036@gmail.com>
2024-06-24 17:35:19 +0200

    Added deleteEmployee endpoint

--- routes/employees.js
+++ routes/employees.js
@@ -6,15 +6,27 @@ const router = express.Router();
 router.post('/addEmployee', async (req, res) => {
     console.log("request recieved", req.body);
     try {
-        
         const employeeData = req.body;
         const newEmployee = new Employee(employeeData);
-        console.log("newEmployee", newEmployee);
         await newEmployee.save(); 
-        
         res.status(201).json({ message: 'Employee added successfully', newEmployee });
     } catch (error) {
         res.status(400).json({ message: 'Error adding employee', error: error.message });
+    }
+});
+
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const employee = await Employee.findById(id);
+        if (!employee) {
+            return res.status(404).json({ message: 'Employee not found' });
+        }
+
+        await Employee.deleteOne({ _id: id });
+        res.status(200).json({ message: 'Employee deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: 'Error deleting employee', error: error.message });
     }
 });
 
